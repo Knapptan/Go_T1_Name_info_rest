eb20c8120b8d6f2fb1c0bad34ff7c29e1368c971
knapptan <nikitas@live.ru>
2025-04-24 16:17:05 +0300

    add cfg parser

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"effective-mobile/internal/config"
+)
+
 func main() {
-	
+
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("invalid pars config")
+	}
+
+	fmt.Printf("%w", cfg)
+
+	// r := chi.NewRouter()
+	// r.Route("/api/v1/people", func(r chi.Router) {
+	// 	r.Post("/", h.CreatePerson)
+	// 	r.Get("/", h.GetPeople)
+	// 	r.Put("/{id}", h.UpdatePerson)
+	// 	r.Delete("/{id}", h.DeletePerson)
+	// })
 }
--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,14 @@
 package models
+
+type DBConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DbName   string
+}
+
+type Config struct {
+	Port     string
+	DBConfig DBConfig
+}
